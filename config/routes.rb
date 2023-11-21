59038fa51568e03e5f839c81e846f23a6cfa1aeb
Alexander Da Costa <bsmuse@tutamail.com>
2023-11-20 19:44:48 -0700

    Add the ability to add categories

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,13 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, except: [:edit, :update, :show]
   end
+
+  # namespace :admin do
+  #   root to: 'dashboard#show'
+  #   resources :categories, except: [:edit, :update, :show]
+  # end
 
   get '/about', to: 'about#index'
 
