e68e0252bc59983b9c233331ca232432dc15710c
Alexander Da Costa <bsmuse@tutamail.com>
2023-11-22 14:58:09 -0700

    installed testing sweet

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,5 @@ class User < ApplicationRecord
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true
   validates :password, length: { minimum: 6 }, allow_nil: true
-  # Other validations you might need...
-
-  # Optional: If you want to format the email to be lowercase before saving
   before_save { self.email = email.downcase }
 end
